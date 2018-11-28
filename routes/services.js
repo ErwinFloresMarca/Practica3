4fd07d6ef59caf36628c020485390dd7c0914ba2
mariaSacaca <nanimes12@gmail.com>
2018-11-28 17:24:34 -0400

    respuesta de la pregunta 1 insciso b

--- routes/services.js
+++ routes/services.js
@@ -19,6 +19,7 @@ router.get('/', function(req, res) {
   });
 });
 
+
 router.get('/respB', function(req, res) {
   var respuesta = "originalUrl devuleve la url original de la solicitud original ,este nos permite reescribir req.url libremente ";
   res.status(200).json({
@@ -26,6 +27,29 @@ router.get('/respB', function(req, res) {
   });
 });
 
+router.post('/interes',function(req,res,next) {
+      var montoinicial=req.body.montoinicial;
+      var Ianual=req.body.Ianual;
+      if(montoinicial==0 || Ianual==0 || montoinicial==undefined || Ianual== undefined){
+        res.status(400).json({msm: "error en las variables"});
+      }
+      var a =parseFloat(montoinicial);
+      var b=parseFloat(Ianual);
+      var montofinal=0;
+      montofinal=((b/100)*a)+(a);
+      var anual=0;
+      anual=(b*100);
+      var mensual=anual/12;
+      var semanal=anual/52;
+      var diario=anual/365;
+      res.status(400).json({
+        "montofinal":montofinal,
+        "monto anual":Ianual,
+        "monto mensual":mensual,
+        "monto semanal":semanal,
+        "monto diario":diario
+      });
+  });
 //respondan los siguientes incisos
 
 module.exports = router;
